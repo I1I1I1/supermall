6667908d85210e5f4165e6ada43898964b3cb40c
I1I1I1 <1078523527@qq.com>
2020-09-27 18:32:30 +0800

    目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-
+    <router-view></router-view>
+    <main-tar-bar></main-tar-bar>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  components: {
+  import MainTarBar from 'components/contents/maintarbar/MainTarBar'
+  export default {
+    name: 'app',
+    components: {
+        MainTarBar
+    }
   }
-}
 </script>
 
 <style>
-
+  /*css 引入方式*/
+  @import "assets/css/base.css";
 </style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	configureWebpack:{
+		resolve:{
+			extensions:[],
+			alias:{  //别名
+				'assets':'@/assets',
+				'common':'@/common',
+				'components':'@/components',
+				'network':'@/network',
+				'views':'@/views',
+			} 
+		}
+	}
+}
